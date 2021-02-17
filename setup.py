550b59619697ee06fe53a1c039b713bcb9a232f5
Paulo Ortolan (ext.) <Paulo.Ortolan@ctw.bmwgroup.com>
2021-02-17 11:32:49 +0000

    Adding egg file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-maze-api',
+    version='1.0.0',
+    url='https://github.com/ortolanph/py_maze_api.git',
+    author='Paulo Ortolan',
+    author_email='paulo.ortolan@gmail.com',
+    description='A simple maze API creted using the Hunt and Kill algorithm',
+    packages=find_packages(),
+    install_requires=[],
+)
